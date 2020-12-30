f1fcae14a71fda84933a6f426c638867544368d1
vishnu-hue <66674757+vishnu-hue@users.noreply.github.com>
2020-12-30 13:38:00 +0530

    Update lm_scorer.h

--- ctcdecode/src/lm_scorer.h
+++ ctcdecode/src/lm_scorer.h
@@ -10,8 +10,12 @@
 #include "lm/virtual_interface.hh"
 #include "lm/word_index.hh"
 #include "util/string_piece.hh"
-
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "path_trie.h"
+
+namespace py = pybind11;
+using namespace py::literals;
 
 const double OOV_SCORE = -1000.0;
 const std::string START_TOKEN = "<s>";
