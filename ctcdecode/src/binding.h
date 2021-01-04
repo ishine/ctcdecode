54ffc411e0894567da48ad7c24910b9116d99bb6
vishnu-hue <66674757+vishnu-hue@users.noreply.github.com>
2021-01-04 11:35:37 +0530

    Update binding.h

--- ctcdecode/src/binding.h
+++ ctcdecode/src/binding.h
@@ -1,5 +1,18 @@
-int paddle_beam_decode(THFloatTensor *th_probs,
-                       THIntTensor *th_seq_lens,
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <torch/torch.h>
+#include <memory>
+#include "lm_scorer.h"
+#include "ctc_beam_search_decoder.h"
+#include "utf8.h"
+#include "boost/shared_ptr.hpp"
+#include "boost/python.hpp"
+#include "boost/python/stl_iterator.hpp"
+
+int paddle_beam_decode(at::Tensor th_probs,
+                       at::Tensor th_seq_lens,
                        std::vector<std::string> labels,
                        int vocab_size,
                        size_t beam_size,
@@ -8,14 +21,14 @@ int paddle_beam_decode(THFloatTensor *th_probs,
                        size_t cutoff_top_n,
                        size_t blank_id,
                        int log_input,
-                       THIntTensor *th_output,
-                       THIntTensor *th_timesteps,
-                       THFloatTensor *th_scores,
-                       THIntTensor *th_out_length);
+                       at::Tensor th_output,
+                       at::Tensor th_timesteps,
+                       at::Tensor th_scores,
+                       at::Tensor th_out_length);
 
 
-int paddle_beam_decode_lm(THFloatTensor *th_probs,
-                          THIntTensor *th_seq_lens,
+int paddle_beam_decode_lm(at::Tensor th_probs,
+                          at::Tensor th_seq_lens,
                           std::vector<std::string> labels,
                           int vocab_size,
                           size_t beam_size,
@@ -23,12 +36,12 @@ int paddle_beam_decode_lm(THFloatTensor *th_probs,
                           double cutoff_prob,
                           size_t cutoff_top_n,
                           size_t blank_id,
-                          bool log_input,
-                          int *scorer,
-                          THIntTensor *th_output,
-                          THIntTensor *th_timesteps,
-                          THFloatTensor *th_scores,
-                          THIntTensor *th_out_length);
+                          int log_input,
+                          void *scorer,
+                          at::Tensor th_output,
+                          at::Tensor th_timesteps,
+                          at::Tensor th_scores,
+                          at::Tensor th_out_length);
 
 int is_character_based(void *scorer);
 size_t get_max_order(void *scorer);
